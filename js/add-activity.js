1eee4c9bfe29b6f2bd5cdc8f10d6708dd0698d1a
Jordi <byardenb@gmail.com>
2024-06-25 23:23:58 +0300

    Add activity to archive

--- js/add-activity.js
+++ js/add-activity.js
@@ -1,36 +1,37 @@
+const addFileIconElement = document.getElementById("file-add-icon");
+const uploadElement = document.getElementById("upload");
 
+addFileIconElement.onclick = () => {
+  uploadElement.click();
+};
 
-document.addEventListener('DOMContentLoaded', function () {
-    // Existing file upload click handlers
-    document.getElementById('file-add-icon').addEventListener('click', function () {
-        document.getElementById('upload').click();
-    });
-    document.querySelector('label[for="upload"]').addEventListener('click', function () {
-        document.getElementById('upload').click();
-    });
+document
+  .querySelector('label[for="upload"]')
+  .addEventListener("click", function () {
+    document.getElementById("upload").click();
+  });
 
+const addToArchiveButton = document.getElementById("add-activity-arcive");
+addToArchiveButton.onclick = () => {
+  const name = document.getElementById("name").value;
+  const type = document.getElementById("type").value;
+  const description = document.getElementById("description").value;
+  const target = document.getElementById("target").value;
+  const unit = document.getElementById("unit").value;
+  const isGroupActivity = document.getElementById("groupActivity").checked;
+  const upload = document.getElementById("upload").files[0];
 
-    const addToArchiveButton = document.getElementById('add-activity-arcive');
-    addToArchiveButton.addEventListener('click', function () {
-        const name = document.getElementById('name').value;
-        const category = document.getElementById('category').value;
-        const description = document.getElementById('description').value;
-        const target = document.getElementById('target').value;
-        const unit = document.getElementById('unit').value;
-        const groupActivity = document.getElementById('groupActivity').checked;
-        const upload = document.getElementById('upload').files[0];
+  const formData = {
+    name: name,
+    type: type,
+    description: description,
+    target: {
+      value: target,
+      unit: unit,
+    },
+    frameworkType: isGroupActivity ? COLLECTIVE : PERSONAL,
+  };
 
-
-        const formData = {
-            name: name,
-            category: category,
-            description: description,
-            target: target,
-            unit: unit,
-            groupActivity: groupActivity,
-            upload: upload
-        };
-
-        console.log('Add to Archive:', formData);
-    });
-});
+  addActivity(formData);
+  console.log("Add to Archive:", formData);
+};
