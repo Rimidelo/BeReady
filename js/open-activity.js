a9b97b156c20490f5a70f399fff722b634ac67ce
Jordi <byardenb@gmail.com>
2024-06-30 00:37:42 +0300

    Set edit capability to activity element

--- js/open-activity.js
+++ js/open-activity.js
@@ -88,11 +88,12 @@ const buildModalElement = (mode, activityData) => `
     </div>
   </div>`;
 
-const getActivityFormData = () => {
+const getActivityFormData = (activityList) => {
   const activityFormElement = document.getElementById("activity-form");
   const activityFormData = new FormData(activityFormElement);
 
   return {
+    ...activityList,
     name: activityFormData.get("name"),
     type: activityFormData.get("type"),
     description: activityFormData.get("description"),
@@ -139,7 +140,7 @@ const openActivityModal = (mode, activityData) => {
   activityForm.onsubmit = (event) => {
     event.preventDefault();
     const action = event.submitter.getAttribute("data-action");
-    const formData = getActivityFormData();
+    const formData = getActivityFormData(activityData);
 
     mode[action]?.(formData, activityModal);
     activityModal.hide();
