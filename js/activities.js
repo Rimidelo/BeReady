bd2e629599abd86bd69d766be10ea88d1b59663e
‪Vladimir Lihatchov‬‏ <lichvladimir@gmail.com>
2024-07-26 19:05:33 +0300

    end of html css structure of the plan view

--- js/activities.js
+++ js/activities.js
@@ -266,8 +266,5 @@ const setAddActivityBtnOnClick = () => {
   }
 };
 
-document.getElementById('hamburger-menu').addEventListener('click', function () {
-  const sidebar = document.getElementById('sidebar');
-  sidebar.classList.toggle('active');
-});
+
 
---
+++ js/utili.js
@@ -0,0 +1,4 @@
+document.getElementById('hamburger-menu').addEventListener('click', function () {
+    const sidebar = document.getElementById('sidebar');
+    sidebar.classList.toggle('active');
+  });
