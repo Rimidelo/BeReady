91d8feac504fd179970dd22f7d8a8eef8dedc4f7
Jordi <byardenb@gmail.com>
2024-07-27 22:30:13 +0300

    add profile functionality template

---
+++ js/profile.js
@@ -0,0 +1,22 @@
+const PROFILE_STATUS = {
+  PERSONAL_DETAILS: {
+    name: "פרטים אישיים",
+    isFilled: true,
+  },
+  PREFERENCES: {
+    name: "שאלון אישי",
+    isFilled: true,
+  },
+  FIRST_ORDER_DETAILS: {
+    name: "נתוני צו ראשון",
+    isFilled: false,
+  },
+};
+
+window.onload = async () => {};
+
+const createProfileStatusItem = () => {};
+
+const fetchProfileStatus = (id) => {};
+
+const fetchProfileImage = (id) => {};
